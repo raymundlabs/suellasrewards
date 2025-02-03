2b84370ba898ded00e91c8e6294ea5b3545d97cf
Raymund <raymundnilo1115@gmail.com>
2025-02-03 20:13:44 +0800

    updated runner

--- lib/design/start.dart
+++ lib/design/start.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:google_fonts/google_fonts.dart';
+
 import 'package:suellas/utils.dart';
 import 'package:flutter/gestures.dart';
 import 'dart:ui';
